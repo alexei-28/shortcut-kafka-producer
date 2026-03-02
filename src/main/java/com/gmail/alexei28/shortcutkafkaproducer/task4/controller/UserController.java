6a110eeec37bd725520c579d1358b824ea425651
Alexei <a_subscriber@mail.ru>
2026-03-02 11:18:46 +0200

    feat: Задача 4 - Битые сообщения из CFT

---
+++ src/main/java/com/gmail/alexei28/shortcutkafkaproducer/task4/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.gmail.alexei28.shortcutkafkaproducer.task4.controller;
+
+import com.gmail.alexei28.shortcutkafkaproducer.task4.producer.UserProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+  private final UserProducer userProducer;
+  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+  public UserController(UserProducer userProducer) {
+    this.userProducer = userProducer;
+  }
+
+  /**
+   * Sends a Task2Dto object. E.g. POST http://localhost:8080/api/v1/user/create
+   *
+   * <p>Spring Boot auto convert JSON to Task2Dto
+   */
+  @PostMapping("/create")
+  public String sendMessage(@RequestBody String userJson) {
+    // Отправляем сырую JSON строку
+    userProducer.sendRaw(userJson);
+    return "Message sent to Kafka!";
+  }
+}
