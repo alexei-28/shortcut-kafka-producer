0ec19e0302c8436f79fb1b8ece5d7bc5511a7964
Alexei <a_subscriber@mail.ru>
2026-03-10 16:43:05 +0200

    feat: Задача 4 - Битые сообщения из CFT

--- src/main/java/com/gmail/alexei28/shortcutkafkaproducer/Main.java
+++ src/main/java/com/gmail/alexei28/shortcutkafkaproducer/Main.java
@@ -17,8 +17,8 @@ public class Main {
     SpringApplication app = new SpringApplication(Main.class);
     app.addListeners(new VersionInfoListener());
     context = app.run(args);
-
-    logger.info("Application started successfully!");
+    logger.info(
+        "\n\n ===== Application started successfully! =====\n Задача 4 - Битые сообщения из CFT");
     logger.info(
         "Java version: {}, Java vendor: {}",
         System.getProperty("java.version"),
