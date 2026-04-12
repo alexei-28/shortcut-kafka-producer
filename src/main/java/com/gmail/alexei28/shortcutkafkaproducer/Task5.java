d725e96904baf48a5eceed26258b6423b65d1c0e
Alexei <a_subscriber@mail.ru>
2026-04-12 15:35:15 +0300

    feat: Refactor

--- src/main/java/com/gmail/alexei28/shortcutkafkaproducer/Task5.java
+++ src/main/java/com/gmail/alexei28/shortcutkafkaproducer/Task5.java
@@ -9,12 +9,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 // Validate is application is up: http://localhost:8081/api/v1/actuator/health
 @SpringBootApplication
-public class Main {
+public class Task5 {
   private static ConfigurableApplicationContext context;
-  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+  private static final Logger logger = LoggerFactory.getLogger(Task5.class);
 
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Main.class);
+    SpringApplication app = new SpringApplication(Task5.class);
     app.addListeners(new VersionInfoListener());
     context = app.run(args);
     logger.info("\n\n ===== Application started successfully! =====\n Задача 5 - DLQ, но удобнее");
